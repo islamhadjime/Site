b100b0c88045f48fc62840997934bd2e26b1675b
islamhadjime <islam.hadjime@gmail.com>
2020-02-16 23:54:47 +0300

    1) Примерный проект

---
+++ filmApp/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+
+#
+# site Shpargalka
+# new user redist
+# new user = url/user/aggaunt
+# home_User  = char + text + time + delet + otr +  max > 70
+# user > site/home
+# home = dizain + form + kamer + tri_Search
+# plus site - search , speed , cooki,kamer
